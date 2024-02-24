d96c615d7ce9b924f4b3fcf7f903c861c2932d37
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-02-24 01:24:13 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -11,33 +11,25 @@ window.onload = function() {
 
   const button = document.querySelector("#send-info");
   button.addEventListener("click", sendEmails);
-
-  // const toastLiveExample = document.getElementById("liveToast");
-  // if (button) {
-  //   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(
-  //     toastLiveExample
-  //   );
-  //   button.addEventListener("click", () => {
-  //     toastBootstrap.show();
-  //   });
-  // }
 };
 
-const sendEmails = () => {
+async function sendEmails() {
   const name = document.querySelector("#name-input").value;
   const email = document.querySelector("#email-input").value;
   const message = document.querySelector("#message-input").value;
 
-  emailjs.send("service_o73wfye", "template_glv2v6a", {
+  const result = await emailjs.send("service_o73wfye", "template_glv2v6a", {
     to_name: name,
     to_email: email,
   });
+  console.log(result);
 
-  emailjs.send("service_o73wfye", "template_emjcl2t", {
+  const result2 = await emailjs.send("service_o73wfye", "template_emjcl2t", {
     from_name: name,
     from_email: email,
     message: message,
   });
+  console.log(result2);
 
   document.querySelector("#name-input").value = "";
   document.querySelector("#email-input").value = "";
@@ -46,4 +38,4 @@ const sendEmails = () => {
   const toastLiveExample = document.getElementById("liveToast");
   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
   toastBootstrap.show();
-};
+}
