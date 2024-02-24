17e22787cb7aca8fd2b2efefdd09ade06990d602
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-02-24 00:56:11 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
 /* eslint-disable */
-import "bootstrap";
+import * as bootstrap from "bootstrap";
+window.bootstrap = bootstrap;
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  emailjs.init({
+    publicKey: "lK60Ot8VEDjuTdBha",
+  });
+
+  const button = document.querySelector("#send-info");
+  button.addEventListener("click", sendEmails);
+
+  // const toastLiveExample = document.getElementById("liveToast");
+  // if (button) {
+  //   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(
+  //     toastLiveExample
+  //   );
+  //   button.addEventListener("click", () => {
+  //     toastBootstrap.show();
+  //   });
+  // }
+};
+
+const sendEmails = () => {
+  const name = document.querySelector("#name-input").value;
+  const email = document.querySelector("#email-input").value;
+  const message = document.querySelector("#message-input").value;
+
+  emailjs.send("service_o73wfye", "template_glv2v6a", {
+    to_name: name,
+    to_email: email,
+  });
+
+  emailjs.send("service_o73wfye", "template_emjcl2t", {
+    from_name: name,
+    from_email: email,
+    message: message,
+  });
+
+  document.querySelector("#name-input").value = "";
+  document.querySelector("#email-input").value = "";
+  document.querySelector("#message-input").value = "";
+
+  const toastLiveExample = document.getElementById("liveToast");
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastBootstrap.show();
 };
